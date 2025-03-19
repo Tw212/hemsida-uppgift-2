45ecf6e0d15e6e5a3d2184e9ebc3262cd954c4c8
Tw212 <Tewe07001@utb.vaxjo.se>
2025-03-19 16:36:44 +0100

    SAW

--- funktioner.js
+++ funktioner.js
@@ -1,7 +1,5 @@
 let a = true;
 function navshow(){
-
-    if(a = true){
     document.getElementById("navjs").style.display = "block"
     document.getElementById("omdome").style.display = "none"
     document.getElementById("mainone").style.display = "none"
@@ -9,17 +7,18 @@ function navshow(){
     document.getElementById("sectionmaintwo").style.display = "none"
     document.getElementById("hed").style.display = "none"
     document.getElementById("fot").style.display = "none"
-    a = false;
-    }
-
-    else if(a = false){
-        document.getElementById("navjs").style.display = "none"
-        document.getElementById("omdome").style.display = "block"
-        document.getElementById("mainone").style.display = "block"
-        document.getElementById("sectionmain").style.display = "block"
-        document.getElementById("sectionmaintwo").style.display = "block"
-        document.getElementById("hed").style.display = "block"
-        document.getElementById("fot").style.display = "block"
-        a = true
-    }
+    document.getElementById("navknapp").style.display = "none"
+    document.getElementById("stangknapp").style.display = "block"
+}
+let b = true;
+function stang(){
+            document.getElementById("navjs").style.display = "none"
+            document.getElementById("omdome").style.display = "block"
+            document.getElementById("mainone").style.display = "block"
+            document.getElementById("sectionmain").style.display = "block"
+            document.getElementById("sectionmaintwo").style.display = "block"
+            document.getElementById("hed").style.display = "block"
+            document.getElementById("fot").style.display = "block"
+            document.getElementById("navknapp").style.display = "block"
+            document.getElementById("stangknapp").style.display = "none"
 }
